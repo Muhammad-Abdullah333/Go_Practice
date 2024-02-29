bd7ae776a609df2b618fed057ea0bbf15ba66a1b
Muhammad Abdullah <writetomuhammadabdullah@gmail.com>
2024-02-29 19:54:48 +0500

    My first go commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Main Function")
+	displayAllBlocks()
+	newBlock()
+	ModifyBlock()
+}
+
+func displayAllBlocks() {
+	fmt.Println("Display All Blocks Function")
+}
+
+func newBlock() {
+	fmt.Println("New Block Function")
+}
+
+func ModifyBlock() {
+	fmt.Println("Modify Block Function")
+}
